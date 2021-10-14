c223d53d1b9de15917d415f5d848e7166b1f5dee
Niharika <niharika.dayama@coditas.com>
2021-10-14 13:35:37 +0530

    new branch to test

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  
+    plugins: [
+   [
+         'module-resolver',
+         {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+             tests: ['./tests/'],
+             "components": "./src/components",
+             "theme":"./src/theme",
+             "assets":"./src/assets",
+             "constants":"./src/constants"
+           }
+        }
+      ]
+    ]
+  
 };
