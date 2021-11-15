4591febc27f4be578334d5514aec0fa91c0cb959
niharikadayama <61003868+niharikadayama@users.noreply.github.com>
2021-11-15 11:45:55 +0530

    persist data

--- App.tsx
+++ App.tsx
@@ -1,19 +1,20 @@
-import 'react-native-gesture-handler'
 import React from 'react';
-import store from './src/redux/store'
+import reduxStore from './src/redux/store'
 import {Provider} from 'react-redux'
+import MainStackNavigator from './src/navigation/stackNavigation'
+import { PersistGate } from 'redux-persist/integration/react'
 import { NavigationContainer } from '@react-navigation/native';
-import MainStackNavigator from './src/navigation/stackNavigation';
-
 
 const App = () =>{
+  const{store, persistor} = reduxStore();
+
   return(
     <Provider store={store}>
-     
-      <NavigationContainer>
-        <MainStackNavigator/>
-      </NavigationContainer>
-     
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <MainStackNavigator/>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   )
 }
