d529f73f9809e9b115661ce6da045d41c4570d72
Niharika <niharika.dayama@coditas.com>
2022-02-08 12:07:08 +0530

    cart-screen

--- src/screen/cart/index.tsx
+++ src/screen/cart/index.tsx
@@ -1,62 +1,68 @@
-import React, {useState,useEffect} from "react";
-import {View,Text,FlatList} from 'react-native'
-import styles from "./styles";
-import {Header,CartCard,Button} from "components"
-import {useSelector} from "react-redux";
+import React, {useState, useEffect} from 'react';
+import {View, Text, FlatList} from 'react-native';
+import styles from './styles';
+import {Header, CartCard, Button} from 'components';
+import {useSelector} from 'react-redux';
 
-const Cart = ({navigation,route}) =>{
-    const item = route.params
-    const [totalPrice,setTotalPrice] = useState(0);
-    const [totalItem,setTotalItem] = useState(0);
-    const myCart = useSelector((state)=>state.shop.cart)
-   
-    useEffect(()=>{
-      let items = myCart.reduce((items,item)=>items+item.qty,0);
-      let price = myCart.reduce((price,item)=>price += item.qty * item.price,0);
-      setTotalItem(items);
-      setTotalPrice(price);
-    },[myCart,totalItem,totalPrice,setTotalItem,setTotalPrice])
+const Cart = ({navigation, route}) => {
+  const item = route.params;
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalItem, setTotalItem] = useState(0);
+  const myCart = useSelector(state => state.shop.cart);
 
-    return (
-     <View style={styles.container}>
-        
-           <Header 
-               leftIcon={"chevron-back"} 
-               rightIcon={"person-circle-sharp"} 
-               iconSize={30}
-               onLeftIconPress ={()=>{ navigation.navigate('root') }}
-               onRightIconPress ={()=>{ navigation.navigate('profile') }}
-               showLogo= {false} 
-               title={''}
-            />
+  useEffect(() => {
+    let items = myCart.reduce((items, item) => items + item.qty, 0);
+    let price = myCart.reduce(
+      (price, item) => (price += item.qty * item.price),
+      0,
+    );
+    setTotalItem(items);
+    setTotalPrice(price);
+  }, [myCart]);
 
-           <View style={styles.titleView}>
-                <Text style={styles.title}>My Bag</Text>
-                <Text style={styles.subTitle}>Check and Pay your Item</Text>
-           </View>
-               
-            <FlatList 
-               data={myCart}
-               keyExtractor={(item,index)=>index.toString()}
-               renderItem = { ( {item}) =>{
-                   return(
-                    <CartCard itemData={item}/>  
-                   )
-               }}
-            />
-            
-            <View style={styles.totalCard}>
-               <Text style={styles.totalCardText}>{totalItem} items</Text>
-               <Text style={styles.totalCardText}>{`Rs. ${totalPrice}`}</Text>
-            </View>
+  return (
+    <View style={styles.container}>
+      <Header
+        leftIcon={'chevron-back'}
+        rightIcon={'person-circle-sharp'}
+        iconSize={30}
+        onLeftIconPress={() => {
+          navigation.navigate('root');
+        }}
+        onRightIconPress={() => {
+          navigation.navigate('profile');
+        }}
+        showLogo={false}
+        title={''}
+      />
 
-            <Button 
-               name = {'Checkout'}  
-               theme={'primary'} 
-               onPress={()=>{navigation.navigate('Home')}}
-            />
-     </View>
-    )
-}
+      <View style={styles.titleView}>
+        <Text style={styles.title}>My Bag</Text>
+        <Text style={styles.subTitle}>Check and Pay your Item</Text>
+      </View>
+
+      <FlatList
+        data={myCart}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => {
+          return <CartCard itemData={item} />;
+        }}
+      />
+
+      <View style={styles.totalCard}>
+        <Text style={styles.totalCardText}>{totalItem} items</Text>
+        <Text style={styles.totalCardText}>{`Rs. ${totalPrice}`}</Text>
+      </View>
+
+      <Button
+        name={'Checkout'}
+        theme={'primary'}
+        onPress={() => {
+          navigation.navigate('Home');
+        }}
+      />
+    </View>
+  );
+};
 
 export default Cart;
